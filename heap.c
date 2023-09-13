bbb7bbd3301e1443fd67f9a232c4396fd9ea24bb
Renee Joo <renee.joo03@gmail.com>
2023-09-13 10:07:02 +0900

    I'm tired af It's 10am and I slept for only 4hrs

--- heap.c
+++ heap.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 #define MaxElement 200
 
@@ -7,9 +8,22 @@ typedef struct {
 }element;
 
 typedef struct {
-	element  heap[MaxElement];
+	element heap[MaxElement];
 	int HeapSize;
 }HeapType;
 
-HeapType heap;
-//HeapType* heap = create;
+HeapType* create() {	//heaptype만큼 동적 메모리 할당_삭제함수나 삽입함수 구현을 위해 필요
+
+	return (HeapType*)malloc(sizeof(HeapType));
+}
+
+//HeapType heap;	//정적할당
+HeapType* heap = create;	//동적할당
+
+//최대 히프_insert 함수_업힙(자식노드->부모노드 비교해야해서)
+
+//insert min heap 함수
+
+//삭제함수(다운힙 부모노드->자식노드)
+
+//delete min heap 함수
