28a39d20243d12b36a5f7908f92823a9b9dfed9f
Renee Joo <renee.joo03@gmail.com>
2023-09-14 00:06:47 +0900

    삽입함수 구현

--- heap.c
+++ heap.c
@@ -1,6 +1,5 @@
 #include<stdio.h>
 #include<stdlib.h>
-
 #define MaxElement 200
 
 typedef struct {
@@ -8,22 +7,59 @@ typedef struct {
 }element;
 
 typedef struct {
-	element heap[MaxElement];
-	int HeapSize;
+	int heap[MaxElement];
+	int HeapSize;	//heap에 몇개의 요소가 저장되어있는지
 }HeapType;
 
-HeapType* create() {	//heaptype만큼 동적 메모리 할당_삭제함수나 삽입함수 구현을 위해 필요
+HeapType* create() {	//heaptype만큼 동적 메모리 할당(삭제함수나 삽입함수 구현을 위해 필요)+ 초기화
 
-	return (HeapType*)malloc(sizeof(HeapType));
+	HeapType* heap= (HeapType*)malloc(sizeof(HeapType));
+	
+	if (heap == NULL) {
+		printf("메모리가 할당되지 않았습니다");
+		return heap;
+	}
+
+	heap->HeapSize = 0;
 }
 
 //HeapType heap;	//정적할당
-HeapType* heap = create;	//동적할당
+//HeapType* heap = create;	//동적할당
 
-//최대 히프_insert 함수_업힙(자식노드->부모노드 비교해야해서)
+void InsertMax(HeapType* h, int item) {	//새 노드 저장
 
-//insert min heap 함수
+	int where;	//새로운 노드를 저장할 위치
+	where = ++(h->HeapSize);	//가장 마지막 자리에 저장
 
-//삭제함수(다운힙 부모노드->자식노드)
+	while ((where != 1) && (item > h->heap[where / 2])) {	//where가 1이 아니고(더 올라갈 곳 X) && 부모노드보다 클때
 
-//delete min heap 함수
+		h->heap[where] = h->heap[where / 2];	//자신의 부모노드가 있는 곳으로 올라감
+		where /= 2;		//저장장소도 갱신
+	}
+	h->heap[where] = item;
+}
+
+
+void main() {
+
+	int e1 = 10, e2 = 20, e3 = 30;
+
+	HeapType* heap;
+
+	heap = create();	//히프 동적 할당
+
+	InsertMax(heap, e1); InsertMax(heap, e2); InsertMax(heap, e3);	//삽입
+	/*
+			30
+		   /  \
+		  10  20
+	*/
+
+	for (int num = 1; num <heap->HeapSize+1 ; num++) {
+
+		printf("%d ", heap->heap[num]);
+	}
+
+	free(heap);
+
+}
